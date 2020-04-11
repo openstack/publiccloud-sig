a670331b47fd21078ed8dd4f26d526c2e0cbb99e
Andreas Jaeger <aj@suse.com>
2020-04-11 16:18:29 +0200

    Cleanup py27 support and docs
    This repo is now testing only with Python 3, so let's make
    a few cleanups:
    - Remove python 2.7 stanza from setup.py
    - Remove obsolete sections from setup.cfg
    - Cleanup doc/source/conf.py to remove now obsolete content.
    - Use newer openstackdocstheme version
    - Remove install_command from tox.ini, the default is fine
    - Remove unused file test-requirements.txt
    - Remove unused dependencies from requirements.txt, this repo
      is docs only
    
    Change-Id: I05c12768dd1648c59156bc8700238aaa59ee1dc3

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -12,16 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath('../..'))
 # -- General configuration ----------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
-    'sphinx.ext.autodoc',
     'openstackdocstheme',
     #'sphinx.ext.intersphinx',
 ]
